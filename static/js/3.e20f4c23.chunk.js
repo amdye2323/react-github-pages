c4587ced7f96811b5cbec91b5713a8885ce76c29
김태영 <gimtaeyeong@gimtaeyeong-ui-MacBookAir.local>
2021-07-25 14:09:50 +0900

    Updates

---
+++ static/js/3.e20f4c23.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[3],{58:function(t,o,i){"use strict";i.r(o),o.default=i.p+"static/media/aboutMySelf.english.9c2b86d6.md"}}]);
+//# sourceMappingURL=3.e20f4c23.chunk.js.map
